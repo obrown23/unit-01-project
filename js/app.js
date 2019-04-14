7b43cf47df8a83aa3bfef9c18add1f2e49ce91ce
KnightRider <omar.brown@comcast.net>
2019-04-14 14:21:10 -0400

    16th commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // hanging chndeleir
+//jQuery
+$('button').click(()=>{
+    event.preventDefault()
+    let input = $('input:text').val()
+    $('ol').append('<li class="strik">'+ input + "</li>")
+    $('input').val('')
+}
+)
+$('ol').on("click", ".strik", ()=>{
+    $(event.target).css('text-decoration','line-through');
+});
